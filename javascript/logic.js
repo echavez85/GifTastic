541fc891342706126c20623883ac63926514a758
echavez85 <echavez85@gmail.com>
2018-11-04 21:16:45 -0600

    Created pseudocode for logic.js.

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,3 +1,48 @@
 $(document).ready(function()) {
+    // Initial gif array
+
+    // displayGifFunction re-renders HTML to display the appropriate content.
+
+    // Create AJAX call for the gif button being clicked.
+
+    // Retrieve URL for the gif.
     
+    // Create an element to hold the gif.
+
+    // Append the gif with a (state === "still").
+
+    // Store the rating data.
+
+    // Create an element to have the rating data displayed.
+
+    // Display the rating.
+
+    // Function for displaying the gif and gif data.
+
+    // Delete other buttons prior to adding new ones.
+
+    // Loop through array of gif topics.
+
+    // Dynamically generate buttons for each gif topic in the array with $("<button").
+
+    // Add class of "gifBtn" to button.
+
+    // Add data attribute.
+
+    // Provide initial button text.
+
+    // Add the button to id="buttonsHolder".
+
+    // Create on click function for submit button.
+
+    // Take input from textbox.
+
+    // Add gif topic from textbox to array.
+
+    // Call renderButtons to process gif array.
+
+    // Add click event listener to all elements with class of "gifBtn".
+
+    // Call renderButtons to display initial buttons.
+
 }
