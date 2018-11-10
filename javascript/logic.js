853967e24354b050b5f1831c488236bb930788c6
echavez85 <echavez85@gmail.com>
2018-11-10 14:50:18 -0600

    Fixed JS w/ Dave's help. Changed data state to still.

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,23 +1,78 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Initial gifTopics array
     var gifTopics = ["Gross", "Party", "Excited", "Sad", "Nerd", "Christmas", "Thanksgiving", "Snow"];
 
     // displayGifFunction re-renders HTML to display the appropriate content.
-    function displayGifButtons() {
-        var gifTopic = $(this).attr("data-name");
+
+    // Function for displaying the buttons.
+    function renderButtons() {
+
+        // Delete other buttons prior to adding new ones.
+        $("#buttonsHolder").empty();
+
+        // Loop through array of gif topics.
+        for (var i = 0; i < gifTopics.length; i++) {
+            // Dynamically generate buttons for each gif topic in the array with $("<button").
+            var topicButton = $("<button>");
+
+            // Add class of "gifBtn" to button.
+            topicButton.addClass("gifBtn");
+
+            // Add data attribute.
+            topicButton.attr("data-name", gifTopics[i]);
+
+            // Provide initial button text.
+            topicButton.text(gifTopics[i]);
+
+            // Add the button to id="buttonsHolder".
+            $("#buttonsHolder").append(topicButton);
+        }
+    }
+
+    $(document).on("click", ".gifBtn", function (event) {
+        var searchParam = $(this).attr('data-name')
+        displayGifButtons(searchParam);
+    })
+
+    $("#add-gif").on("click", function (event) {
+        event.preventDefault();
+        var newTopic = $("#gif-topic-input").val().trim();
+        gifTopics.push(newTopic);
+        renderButtons();
+    })
+
+    // Create on click function for submit button.
+    $("#add-gifs").on("click", function (event) {
+        event.preventDefaul();
+
+        // Take input from textbox.
+        var gifTopic = $("#gif-topic-input").val().trim();
+
+        // Add gif topic from textbox to array.
+        gifTopics.push(gifTopic);
+
+        // Call renderButtons to process gif array.
+
+        renderButtons();
+    });
+
+    function displayGifButtons(gifTopic) {
+        console.log('gifTopic', gifTopic);
+
+        // var gifTopic = $(this).attr("data-name");
         var queryURL = `https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=qPnM9oCUgEYxmEAp0JVH7PdbS4lsMrcv&q=${gifTopic}`;
 
         // Create AJAX request for the gif button being clicked.
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
-
+        }).then(function (response) {
+            console.log(response);
             // Store array of results in the results variable.
             var results = response.data;
             // Loop over every result item.
-            for(var i = 0; i< results.length; i++) {
+            for (var i = 0; i < results.length; i++) {
                 // Create a div for the gif.
                 var gifDiv = $("<div>");
 
@@ -25,66 +80,28 @@ $(document).ready(function() {
                 var topicImage = $("<img>");
 
                 // Give image tag source from the response.
-                topicImage.attr("src", results[i].original.url);
-                
+                topicImage.attr("src", results[i].images.original_still.url);
+
                 // Store rating of the results.
                 var rating = results[i].rating;
 
                 // Create paragraph tag with item's rating.
-                var p = $("<p>").text(`Rating: ${rating}`);
-                
+                var rating = $("<p>").text(`Rating: ${rating}`);
+
                 // Append the gifs to the gifdiv.
                 gifDiv.append(topicImage);
-                givDiv.append(rating);
+                gifDiv.append(rating);
 
                 // Prepend the gifDiv to the html.
                 $("#gifHolder").prepend(gifDiv);
 
-            });
-        }
-
-        // Function for displaying the buttons.
-        function renderButtons() {
-
-            // Delete other buttons prior to adding new ones.
-            $("#buttonsHolder").empty();
-
-            // Loop through array of gif topics.
-            for(var i = 0; i < gifTopics.length; i++)
-            {
-                // Dynamically generate buttons for each gif topic in the array with $("<button").
-                var topicButton = $("<button>");
-
-                // Add class of "gifBtn" to button.
-                topicButton.addClass("gifBtn");
-
-                // Add data attribute.
-                topicButton.attr("data-name", gifTopics[i];
-
-                // Provide initial button text.
-                topicButton.text(gifTopics[i]);
-
-                // Add the button to id="buttonsHolder".
-                $("#buttonsHolder").append(topicButton);
-            }
-        }
-
-        // Create on click function for submit button.
-        $("#add-gifs").on("click", function(event) {
-            event.preventDefaul();
-
-            // Take input from textbox.
-            var gifTopic = $("#gif-topic-input").val().trim();
-
-            // Add gif topic from textbox to array.
-            gifTopics.push(gifTopic);
-
-            // Call renderButtons to process gif array.
-
-            renderButtons();
-        });
-
+            };
+        })
         renderButtons();
+    };
+    renderButtons();
+});
+
 
 
     // Append the gif with a (state === "still").
@@ -93,22 +110,21 @@ $(document).ready(function() {
 
     // Display the rating.
 
-    
 
-    
 
-    
 
-    
 
-    
 
-    
 
-    
+
+
+
+
+
+
+
 
     // Add click event listener to all elements with class of "gifBtn".
 
     // Call renderButtons to display initial buttons.
 
-})
